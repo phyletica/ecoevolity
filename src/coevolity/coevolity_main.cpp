88560a626162c10c648fc43c550a107f5ba3c843
Jamie Oaks <joaks1@gmail.com>
2015-12-16 20:10:57 -0800

    Playing with NCL in coevolity_main.cpp

--- src/coevolity/coevolity_main.cpp
+++ src/coevolity/coevolity_main.cpp
@@ -1,6 +1,32 @@
+#include <ncl/nxsmultiformat.h>
+#include <ncl/nxsreader.h>
+#include <ncl/nxsexception.h>
+#include <ncl/nxspublicblocks.h>
+#include <ncl/nxstaxablock.h>
+#include <ncl/nxsblock.h>
+#include <ncl/nxsstring.h>
+
 #include "util.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout << "Hello World!" << std::endl;
+    MultiFormatReader nexusReader(-1, NxsReader::WARNINGS_TO_STDERR);
+    nexusReader.ReadFilepath(argv[1], MultiFormatReader::NEXUS_FORMAT);
+    
+    int numTaxaBlocks = nexusReader.GetNumTaxaBlocks();
+    std::cout << numTaxaBlocks << " TAXA block(s) read.\n";
+    for (int i = 0; i < numTaxaBlocks; ++i) {
+        NxsTaxaBlock * taxaBlock = nexusReader.GetTaxaBlock(i);
+        std::string taxaBlockTitle = taxaBlock->GetTitle();
+        std::cout << "Taxa block index " << i << " has the Title \"" << taxaBlockTitle << "\"\n";
+    
+        const unsigned nCharBlocks = nexusReader.GetNumCharactersBlocks(taxaBlock);
+        std::cout  <<  nCharBlocks << " CHARACTERS/DATA block(s) refer to this TAXA block\n";
+        for (unsigned j = 0; j < nCharBlocks; ++j) {
+            const NxsCharactersBlock * charBlock = nexusReader.GetCharactersBlock(taxaBlock, j);
+            std::string charBlockTitle = taxaBlock->GetTitle();
+            std::cout << "Taxa block index " << j << " has the Title \"" << charBlockTitle << "\"\n";
+        }
+    }
     return 0;
 }
