17f5cc113ad27ffce031b1ab6cc83bb5770ebb0e
Jamie Oaks <joaks1@gmail.com>
2016-02-02 14:58:24 -0800

    Cleaning up comments in util.hpp

--- src/ecoevolity/util.hpp
+++ src/ecoevolity/util.hpp
@@ -12,8 +12,8 @@
  *
  * If the key does not exist, an out_of_range error is thrown.
  * From:
- *      http://jeetworks.org/safe-and-const-correct-stdmap-access-in-c-stl/
- *      2015 Jeet Sukumaran <http://jeetworks.org
+ *      <http://jeetworks.org/safe-and-const-correct-stdmap-access-in-c-stl/>
+ *      2015 Jeet Sukumaran <http://jeetworks.org>
  */
 template <typename T>
 const typename T::value_type::second_type& map_at(
