4cb1fb3d91daf2e35c037da96fe1f31eb44cdb84
Jamie Oaks <joaks1@gmail.com>
2016-02-03 17:08:16 -0800

    Fixing linking issue with map_at function.
    Moving function definition to header file.

--- src/ecoevolity/util.hpp
+++ src/ecoevolity/util.hpp
@@ -18,7 +18,14 @@
 template <typename T>
 const typename T::value_type::second_type& map_at(
         const T& container,
-        const typename T::value_type::first_type key);
+        const typename T::value_type::first_type key) {
+    typename T::const_iterator it = container.find(key);
+    if (it == container.end()) {
+        throw std::out_of_range("Key not found");
+    }
+    return it->second;
+}
+
 
 std::vector<std::string> & split(
         const std::string &s,
