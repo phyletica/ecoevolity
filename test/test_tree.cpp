401b98c0628b371fd1d793f3e13900994a777aa5
Jamie Oaks <joaks1@gmail.com>
2016-03-12 19:32:49 -0800

    Adding SNAPP likelihood scores on test datasets.

--- test/test_tree.cpp
+++ test/test_tree.cpp
@@ -1,6 +1,14 @@
 #include "catch.hpp"
 #include "ecoevolity/tree.hpp"
 
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// diploid-standard-data-ntax5-nchar5.nex
+// height = 0.01
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// log likelihood = -31.77866581319647
 TEST_CASE("Testing constructor of PopulationTree", "[PopulationTree]") {
 
     SECTION("Testing constructor") {
@@ -10,4 +18,201 @@ TEST_CASE("Testing constructor of PopulationTree", "[PopulationTree]") {
         double l = tree.compute_log_likelihood();
         REQUIRE(l == Approx(-31.77866581319647));
     }
+
 }
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// hemi129.nex 
+// height = 0.01
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// log likelihood             = -248.93254688526213
+// log likelihood correction  = -135.97095011239867
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// height = 0.01
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -7099.716015109998
+// Log likelihood correction = -3317.567573476714
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// dominant
+// height = 0.01
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -6986.120524781545
+// Log likelihood correction = -3317.567573476714
+
+
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// hemi129.nex 
+// height = 0.0
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -328.39238828878365
+// Log likelihood correction = -135.97095011239867
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// height = 0.0
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -7256.501742344454
+// Log likelihood correction = -3317.567573476714
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// dominant
+// height = 0.0
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -7223.362711937651
+// Log likelihood correction = -3317.567573476714
+
+
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// hemi129.nex 
+// height = 0.2
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -227.41048391087554
+// Log likelihood correction = -135.97095011239867
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// height = 0.2
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -7304.180743441677
+// Log likelihood correction = -3317.567573476714
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// dominant
+// height = 0.2
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 1.0
+// v = 1.0
+// Log likelihood            = -7405.145951634711
+// Log likelihood correction = -3317.567573476714
+
+
+
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// hemi129.nex 
+// height = 0.03
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 10.0
+// v = 10.0 / 19.0
+// Log likelihood            = -327.7437811413033
+// Log likelihood correction = -135.97095011239867
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// height = 0.03
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 10.0
+// v = 10.0 / 19.0
+// Log likelihood            = -6472.856486972301
+// Log likelihood correction = -3317.567573476714
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// dominant
+// height = 0.03
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 10.0
+// v = 10.0 / 19.0
+// Log likelihood            = -6494.774924871097
+// Log likelihood correction = -3317.567573476714
+  
+  
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// hemi129.nex 
+// height = 0.03
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 10.0 / 19.0
+// v = 10.0
+// Log likelihood            = -265.0023534261969
+// Log likelihood correction = -135.97095011239867
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// height = 0.03
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 10.0 / 19.0
+// v = 10.0
+// Log likelihood            = -10163.468886613919
+// Log likelihood correction = -3317.567573476714
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// dominant
+// height = 0.03
+// coalescent_rate = 10.0, 10.0, 10.0
+// u = 10.0 / 19.0
+// v = 10.0
+// Log likelihood            = -10999.288193543642
+// Log likelihood correction = -3317.567573476714
+
+
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// hemi129.nex 
+// height = 0.03
+// coalescent_rate = 111.1, 111.1, 111.1
+// u = 10.0 / 19.0
+// v = 10.0
+// Log likelihood            = -224.40177558289847
+// Log likelihood correction = -135.97095011239867
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// height = 0.03
+// coalescent_rate = 111.1, 111.1, 111.1
+// u = 10.0 / 19.0
+// v = 10.0
+// Log likelihood            = -8158.88094671241
+// Log likelihood correction = -3317.567573476714
+
+// BEAST v2.4.0 (master 3731dff6884f7dd27b288099027dc1d500a3a9d8)
+// SNAPP v1.3.0 (master 24d18026c774b10f2e79de16100d96e1a5df1b96)
+// aflp_25.nex 
+// dominant
+// height = 0.03
+// coalescent_rate = 111.1, 111.1, 111.1
+// u = 10.0 / 19.0
+// v = 10.0
+// Log likelihood            = -8034.250341980543
+// Log likelihood correction = -3317.567573476714
