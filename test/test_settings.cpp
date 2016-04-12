9e976768b525a399318b9e5e0e28acdf9abbe272
Jamie Oaks <joaks1@gmail.com>
2016-04-12 13:17:46 -0700

    More testing of settings classes.
    All passing so far.

--- test/test_settings.cpp
+++ test/test_settings.cpp
@@ -68,6 +68,32 @@ TEST_CASE("Testing missing gamma parameter settings", "[ContinuousDistributionSe
     }
 }
 
+TEST_CASE("Testing missing exponential parameter settings", "[ContinuousDistributionSettings]") {
+
+    SECTION("Testing no parameters") {
+        std::string name = "exponential_distribution";
+        std::unordered_map<std::string, double> parameters;
+        REQUIRE_THROWS_AS(ContinuousDistributionSettings(name, parameters),
+                EcoevolityContinuousDistributionSettingError);
+    }
+
+    SECTION("Testing missing parameters") {
+        std::string name = "exponential_distribution";
+        std::unordered_map<std::string, double> parameters;
+        parameters["offset"] = 0.01;
+        REQUIRE_THROWS_AS(ContinuousDistributionSettings(name, parameters),
+                EcoevolityContinuousDistributionSettingError);
+    }
+
+    SECTION("Testing lambda parameter") {
+        std::string name = "exponential_distribution";
+        std::unordered_map<std::string, double> parameters;
+        parameters["lambda"] = 1.0;
+        REQUIRE_THROWS_AS(ContinuousDistributionSettings(name, parameters),
+                EcoevolityContinuousDistributionSettingError);
+    }
+}
+
 TEST_CASE("Testing gamma parameter settings errors", "[ContinuousDistributionSettings]") {
         std::string name = "gamma_distribution";
         std::unordered_map<std::string, double> parameters;
@@ -83,8 +109,16 @@ TEST_CASE("Testing gamma parameter settings errors", "[ContinuousDistributionSet
                 EcoevolityContinuousDistributionSettingError);
 }
 
+TEST_CASE("Testing exponential parameter settings errors", "[ContinuousDistributionSettings]") {
+        std::string name = "exponential_distribution";
+        std::unordered_map<std::string, double> parameters;
+        parameters["rate"] = 0.0;
+        REQUIRE_THROWS_AS(ContinuousDistributionSettings(name, parameters),
+                EcoevolityContinuousDistributionSettingError);
+}
+
 TEST_CASE("Testing gamma settings to string", "[ContinuousDistributionSettings]") {
-    SECTION("Testing gamma 10, 1") {
+    SECTION("Testing gamma to string") {
         std::string name = "gamma_distribution";
         std::unordered_map<std::string, double> parameters;
         parameters["shape"] = 10.0;
@@ -117,5 +151,90 @@ TEST_CASE("Testing gamma settings to string", "[ContinuousDistributionSettings]"
         s = settings.to_string(0);
         e = "gamma_distribution:\n    shape: 0.123457\n    scale: 1.00001\n    offset: 0.001\n";
         REQUIRE(s == e);
+    }
+}
+
+TEST_CASE("Testing exponential settings to string", "[ContinuousDistributionSettings]") {
+    SECTION("Testing exponential to string") {
+        std::string name = "exponential_distribution";
+        std::unordered_map<std::string, double> parameters;
+        parameters["rate"] = 1.0;
+        ContinuousDistributionSettings settings = ContinuousDistributionSettings(name, parameters);
+
+        REQUIRE(settings.get_name() == "exponential_distribution");
+        std::string s = settings.to_string(0);
+        std::string e = "exponential_distribution:\n    rate: 1\n";
+        REQUIRE(s == e);
+
+        parameters.clear();
+        parameters["rate"] = 1.0;
+        parameters["offset"] = 0.01;
+        settings = ContinuousDistributionSettings(name, parameters);
+
+        REQUIRE(settings.get_name() == "exponential_distribution");
+        s = settings.to_string(0);
+        e = "exponential_distribution:\n    rate: 1\n    offset: 0.01\n";
+        REQUIRE(s == e);
+
+        parameters.clear();
+        parameters["rate"] = 0.12345678910;
+        parameters["offset"] = 0.001;
+        settings = ContinuousDistributionSettings(name, parameters);
+
+        REQUIRE(settings.get_name() == "exponential_distribution");
+        s = settings.to_string(0);
+        e = "exponential_distribution:\n    rate: 0.123457\n    offset: 0.001\n";
+        REQUIRE(s == e);
+    }
+}
+
+TEST_CASE("Testing gamma settings to instance", "[ContinuousDistributionSettings]") {
+    SECTION("Testing gamma to instance") {
+        std::string name = "gamma_distribution";
+        std::unordered_map<std::string, double> parameters;
+        parameters["shape"] = 10.0;
+        parameters["scale"] = 1.0;
+        ContinuousDistributionSettings settings = ContinuousDistributionSettings(name, parameters);
+
+        std::shared_ptr<ContinuousProbabilityDistribution> g;
+        g = settings.get_instance();
+
+        REQUIRE(g->get_min() == 0.0);
+        REQUIRE(g->to_string() == "gamma(shape = 10, scale = 1)");
+
+
+        parameters.clear();
+        parameters["shape"] = 10.0;
+        parameters["scale"] = 1.0;
+        parameters["offset"] = 0.01;
+        settings = ContinuousDistributionSettings(name, parameters);
+
+        g = settings.get_instance();
+
+        REQUIRE(g->to_string() == "gamma(shape = 10, scale = 1, offset = 0.01)");
+    }
+}
+
+TEST_CASE("Testing exponential settings to instance", "[ContinuousDistributionSettings]") {
+    SECTION("Testing exponential to instance") {
+        std::string name = "exponential_distribution";
+        std::unordered_map<std::string, double> parameters;
+        parameters["rate"] = 1.0;
+        ContinuousDistributionSettings settings = ContinuousDistributionSettings(name, parameters);
+
+        std::shared_ptr<ContinuousProbabilityDistribution> g;
+        g = settings.get_instance();
+
+        REQUIRE(g->get_min() == 0.0);
+        REQUIRE(g->to_string() == "exp(lambda = 1)");
+
+
+        parameters["rate"] = 10.0;
+        parameters["offset"] = 0.01;
+        settings = ContinuousDistributionSettings(name, parameters);
+
+        g = settings.get_instance();
+
+        REQUIRE(g->to_string() == "exp(lambda = 10, offset = 0.01)");
     }
 }
