30a85307ed1b5c8cdeb8a392e728db6813d0dfc2
Jamie Oaks <joaks1@gmail.com>
2016-12-20 16:05:43 -0600

    Adding another test of RandomNumberGenerator.
    Adding another test of dirichlet_process method; testing version
    that returns a copy of the partition as a vector.

--- test/test_rng.cpp
+++ test/test_rng.cpp
@@ -782,3 +782,65 @@ TEST_CASE("Testing dirichet_process(3, 4.2)", "[RandomNumberGenerator]") {
         }
     }
 }
+
+TEST_CASE("Testing vector return version of dirichet_process(3, 0.6)",
+        "[RandomNumberGenerator]") {
+
+    SECTION("Testing dirichlet_process(3, 0.6)") {
+        unsigned int nsamples = 200000;
+        unsigned int n = 3;
+        double a = 0.6;
+
+        std::map<std::string, int> model_counts;
+        std::map<int, int> nevent_counts = {
+                {1, 0},
+                {2, 0},
+                {3, 0},
+        };
+
+        RandomNumberGenerator rng(342254);
+        for (unsigned int i = 0; i < nsamples; ++i) {
+            std::vector<unsigned int> elements = rng.dirichlet_process(n, a);
+            std::ostringstream stream;
+            std::set<unsigned int> unique_elements;
+            unsigned int max_index = 0;
+            for (auto e: elements) {
+                stream << e;
+                unique_elements.insert(e);
+                if (e > max_index) {
+                    max_index = e;
+                }
+            }
+            unsigned int ncats = unique_elements.size();
+            REQUIRE(max_index + 1 == ncats);
+            ++nevent_counts.at(ncats);
+            std::string model_str = stream.str();
+            if (model_counts.count(model_str) < 1) {
+                model_counts[model_str] = 1;
+            }
+            else {
+                ++model_counts[model_str];
+            }
+        }
+
+        int total = 0;
+        for (auto const &kv: model_counts) {
+            total += kv.second;
+        }
+        REQUIRE(total == nsamples);
+        total = 0;
+        for (auto const &kv: nevent_counts) {
+            total += kv.second;
+        }
+        REQUIRE(total == nsamples);
+
+        REQUIRE(model_counts.at("000") == nevent_counts.at(1));
+        REQUIRE(model_counts.at("012") == nevent_counts.at(3));
+        REQUIRE((model_counts.at("001") + model_counts.at("010") + model_counts.at("011")) == nevent_counts.at(2));
+
+        for (std::string m: {"000", "001", "010", "011", "012"}) {
+            REQUIRE((model_counts.at(m) / (double)nsamples) == Approx(std::exp(
+                    get_dpp_log_prior_probability(m, a))).epsilon(0.002));
+        }
+    }
+}
