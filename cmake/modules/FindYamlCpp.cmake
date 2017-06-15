86f8db6a96710d0620cac4dc1fc46f1e8214bcdb
Jamie Oaks <joaks1@gmail.com>
2017-06-15 17:01:28 -0500

    Updating FindYamlCpp.cmake module.
    It was having trouble finding the locally compiled YAML (compiled with
    build-yaml-cpp.sh script) on some machines. The changes seem to help.

--- cmake/modules/FindYamlCpp.cmake
+++ cmake/modules/FindYamlCpp.cmake
@@ -19,29 +19,12 @@ message(STATUS "YAML_CPP_PREFIX: ${YAML_CPP_PREFIX}")
 set(YAML_CPP_STATIC libyaml-cpp.a)
 # endif()
 
-include (FindPkgConfig)
-
-if (PKG_CONFIG_FOUND)
-    if (YAML_CPP_FIND_VERSION)
-        set (_PACKAGE_ARGS "yaml-cpp>=${YAML_CPP_FIND_VERSION}")
-    else ()
-        set (_PACKAGE_ARGS "yaml-cpp")
-    endif ()
-    if (YAML_CPP_FIND_REQUIRED)
-        set(_PACKAGE_ARGS "${_PACKAGE_ARGS}" REQUIRED)
-    endif ()
-    pkg_check_modules (PC_YAML_CPP "${_PACKAGE_ARGS}")
-    set (YAML_CPP_DEFINITIONS "${PC_YAML_CPP_CFLAGS_OTHER}")
-    set (YAML_CPP_VERSION_STRING "${PC_YAML_CPP_VERSION}")
-endif ()
-
 find_path (YAML_CPP_INCLUDE_DIR
-        NAMES "yaml-cpp/yaml.h"
+        NAMES "yaml-cpp"
         PATH_SUFFIXES "include"
         HINTS
+        "${YAML_CPP_PREFIX}"
         "${YAML_CPP_INCLUDE_PREFIX}"
-        "${PC_YAML_CPP_INCLUDEDIR}"
-        "${PC_YAML_CPP_INCLUDE_DIRS}"
         "${CMAKE_INCLUDE_PATH}"
         /usr/local/include
         /usr/include
@@ -52,13 +35,13 @@ find_path (YAML_CPP_INCLUDE_DIR
         /opt/csw/yaml-cpp/      # Blastwave
         /opt/yaml-cpp/
         )
+
 find_library (YAML_CPP_LIBRARY
         NAMES "${YAML_CPP_STATIC}" "yaml-cpp"
         PATH_SUFFIXES "lib64" "lib"
         HINTS
+        "${YAML_CPP_PREFIX}"
         "${YAML_CPP_LIB_PREFIX}"
-        "${PC_YAML_CPP_LIBDIR}"
-        "${PC_YAML_CPP_LIBRARY_DIRS}"
         "${CMAKE_LIBRARY_PATH}"
         /usr/local
         /usr
