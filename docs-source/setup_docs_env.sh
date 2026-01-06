041c63a05b0e1da6c1def204e5fa9145b13e4321
Jamie R. Oaks <joaks1@gmail.com>
2026-01-06 13:06:07 -0600

    Updating sphinx environment for building docs.
    Switching from pip to conda and adding more Spinx extensions.

--- docs-source/setup_docs_env.sh
+++ docs-source/setup_docs_env.sh
@@ -2,10 +2,31 @@
 
 set -e
 
-echo "Creating Python 3 virtual environment for compiling documentation..."
+# Make sure we leave caller back from whence they called
+current_dir="$(pwd)"
+clean_up() {
+    cd "$current_dir"
+}
+trap clean_up EXIT
 
-python3 -m venv pyenv-docs
-source pyenv-docs/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install wheel
-python3 -m pip install -r python-requirements.txt
+# Get path to directory of this script
+docs_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+conda_env_path="${docs_dir}/conda-env.yml"
+conda_env_name="eco-docs"
+
+conda_exe="conda"
+
+if command -v micromamba >/dev/null 2>&1
+then
+    conda_exe="micromamba"
+elif command -v mamba >/dev/null 2>&1
+then
+    conda_exe="mamba"
+fi
+
+echo ""
+echo "Creating docs conda environment '$conda_env_name' using command:"
+echo '  '"$conda_exe" env create --name "$conda_env_name" --file "$conda_env_path"
+echo ""
+"$conda_exe" env create --name "$conda_env_name" --file "$conda_env_path"
